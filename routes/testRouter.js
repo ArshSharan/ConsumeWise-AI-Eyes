e3e9ea6f2602b2af357ab4073f8124e0b129f0fb
Josh the cat <catterjeeo@gmail.com>
2024-10-01 09:21:02 +0530

    feat: added Docker

--- routes/testRouter.js
+++ routes/testRouter.js
@@ -3,6 +3,6 @@ import { Router } from "express";
 import { testMongoDb } from "../controllers/testController.js"
 
 const router = Router();
-router.get("/mongoDb", testMongoDb);
+// router.get("/mongoDb", testMongoDb);
 
 export default router;
